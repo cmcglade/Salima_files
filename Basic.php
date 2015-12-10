3a912751950c6a380fe952fa64bb431f7ea94a3f
svastani <svastani@gmail.com>
2015-12-10 09:55:11 -0500

    adding files to commit

---
+++ Basic.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+<title> Basic html </title>
+</head>
+<body>
+	<h1> This is php</h1>
+	<p> really? we are doing PHP now? Coooooooooooool!! </p>
+<?php 
+phpinfo ();
+?>
+</body>
